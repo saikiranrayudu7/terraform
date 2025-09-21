64ba43c2f1f0febfac4da208ee4db2ddf1b848c1
saikirun7 <saikirunrayudu@gmail.com>
2025-09-21 23:49:21 +0530

    k

--- dynamic_block_sg/provider.tf
+++ dynamic_block_sg/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "6.8.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "s3-terraform-devops-demo"
+    key            = "sg.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "dynamodb-terraform-demo1"
+  }
+
 }
 
 provider "aws" {
