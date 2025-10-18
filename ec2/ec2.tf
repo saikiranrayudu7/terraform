7364e8f4e555b5aab43e80b61335b7bac310dad4
saikirun7 <saikirunrayudu@gmail.com>
2025-10-18 11:24:36 +0530

    d

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "demo" {
-  ami           = "ami-052064a798f08f0d3"
+  ami           = "ami-0360c520857e3138f" #ubuntu
   instance_type = "t2.micro"
   key_name      = "ansible"
   vpc_security_group_ids = var.sg_type
@@ -9,18 +9,6 @@ resource "aws_instance" "demo" {
     volume_type           = "gp3"
     delete_on_termination = true
   }
-
-  # 👇 Add this block
-  user_data = <<-EOF
-              #!/bin/bash
-              # Wait for 5 minutes (300 seconds)
-              sleep 300
-              # Terminate this instance
-              INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-              REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
-              aws ec2 terminate-instances --instance-ids $INSTANCE_ID --region $REGION
-              EOF
-
   tags = {
     Name = "demo-instance"
   }
